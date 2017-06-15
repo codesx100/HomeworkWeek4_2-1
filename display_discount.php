790f1eb2053f975ffabc9dd8460804d0fc17991f
Oliver Howarth <oah5@njit.edu>
2017-06-15 19:59:55 -0400

    removed htmlspecailcahrs in order to enable html bold and italics on data entry

--- display_discount.php
+++ display_discount.php
@@ -1,17 +1,17 @@
 <?php
 
- $product_description = filter_input(INPUT_POST, 'product_description');
- $list_price= filter_input(INPUT_POST, 'list_price');
- $discount_percent = filter_input(INPUT_POST,'discount percernt');
+$product_description = filter_input(INPUT_POST, 'product_description');
+$list_price= filter_input(INPUT_POST, 'list_price');
+$discount_percent = filter_input(INPUT_POST, 'discount_percent');
 
- $discount_amount = $discount_percent * $list_price * .01;
+ 
+ $discount_amount = $list_price * $discount_percent * .01;
  $discount_price = $list_price - $discount_amount;
 
  $list_price_description = '$'.number_format($list_price,2);
- $discount_percent_description = $discount_percent . '%'
-  $discount_amount_description = number_format($discount_percent,2);
+ $discount_percent_description = $discount_percent."%";
+ $discount_amount_description = number_format($discount_amount,2);
  $discount_price_description = "$".number_format($discount_price,2);
- 
 
 ?>
 <!DOCTYPE html>
@@ -22,22 +22,23 @@
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
+        <h1>Discount Calculator</h1>
 
         <label>Product Description:</label>
-        <span><?php echo htmlspecialchars($product_description); ?></span><br>
+        <span><?php echo ($product_description); ?></span><br>
  
 
         <label>List Price:</label>
-        <span><?php echo htmlspecialchars($list_price); ?></span><br>
+        <span><?php echo ($list_price_description); ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo htmlspecialchars($discount_percent); ?></span><br>
+        <span><?php echo($discount_percent); ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo discount_amount_description; ?></span><br>
+        <span><?php echo $discount_amount_description; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo discount_price_description; ?></span><br>
+        <span><?php echo $discount_price_description; ?></span><br>
     </main>
 </body>
+</html>
